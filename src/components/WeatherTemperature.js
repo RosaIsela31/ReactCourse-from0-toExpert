df70839c4df9e3f15ee87f3513b54cbaead8ae97
Rosa Isela Lopez Palma <rosaiselalopezpalma@air-de-rosa>
2019-12-16 10:54:30 -0600

    using PropTypes for validation and const for temperature

--- src/components/WeatherTemperature.js
+++ src/components/WeatherTemperature.js
@@ -1,9 +1,52 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const WeatherTemperature = () => {
+import WeatherIcon from 'react-icons-weather';
+
+import {
+  CLOUD,
+  CLOUDY,
+  SUN,
+  RAIN,
+  SNOW,
+  WINDY
+} from '../constants/weathers';
+
+const icons = {
+    [CLOUD]: "801",
+    [CLOUDY]: "804",
+    [SUN]: "800",
+    [RAIN]: "200",
+    [SNOW]: "600",
+    [WINDY]: "751",
+}
+
+const getWeatherIcon = weatherState => {
+  const icon = icons[weatherState];
+
+  if(icon)
+      return <WeatherIcon name="owm" iconId={icon} flip="horizontal" rotate="90" />;
+  else
+      return <WeatherIcon name="owm" iconId="800" flip="horizontal" rotate="90" />
+  
+}
+
+const WeatherTemperature = ({ temperature, weatherState }) => {
   return(
-      <div><span> 25ºC </span></div>
+      <div>   
+          {
+              getWeatherIcon(weatherState)
+          }
+          <span> 
+             { `${temperature} ºC` }
+          </span>
+      </div>
   )
+}
+
+WeatherTemperature.propTypes = {
+    temperature: PropTypes.number.isRequired,
+    weatherState: PropTypes.string.isRequired,
 }
 
 
